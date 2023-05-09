b07d91917f203d18f3c407fb1d18cd844c91b8d6
MyNameIsAlpaca <Saragosagabriele@gmail.com>
2023-05-09 17:33:30 +0200

    feat: homepage done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,57 @@
 @extends('layout/main-layout')
 
 @section('content')
-
+<main>
+  <div class="container">
+    <div class="card-container">
+      @foreach ($comics as $item)
+      <div class="card-comics">
+        <div class="img-container">
+          <img src="{{ $item['thumb']}}" alt="">
+        </div>
+        <div class="title">{{ $item['title']}}</div>
+      </div>
+      @endforeach
+    </div>
+    <button>LOAD MORE</button>
+  </div>
+  <div class="link">
+    <div class="main-container">
+      <div class="container">
+          <ul>
+              <li>
+                  <div class="img-container">
+                      <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                  </div>
+                  digital comics
+              </li>
+              <li>
+                <div class="img-container">
+                    <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="">
+                </div>
+                dc merchandise
+              </li>
+              <li>
+                <div class="img-container">
+                    <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="">
+                </div>
+                subscription
+              </li>
+              <li>
+                <div class="img-container">
+                    <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="">
+                </div>
+                comic shop locator
+              </li>
+              <li>
+                <div class="img-container">
+                    <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="">
+                </div>
+                dc power visa
+              </li>
+          </ul>
+      </div>
+    </div>
+  </div>
+</main>
 @endsection
